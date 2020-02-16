01b83e5a7ef5234cae1790cc2c960c6fa3b94485
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-02-16 09:14:46 +0500

    GraphQl, apollo client added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+
+ReactDOM.render(
+  // eslint-disable-next-line react/jsx-filename-extension
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>, document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
