889b117a5681371bf14a7159ad214fae971ba700
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-02-16 09:12:24 +0500

    App.jsx created

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import ErrorSnackbar from 'components/Error/Snackbar';
+import { ApolloClient } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import Routes from './Routes';
+import 'assets/css/index.css';
+
+// eslint-disable-next-line react/prop-types
+export default () => {
+  const [showError, setShowError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    if (errorMessage) {
+      setShowError(true);
+    }
+  }, [errorMessage]);
+
+  const cache = new InMemoryCache();
+
+  const errorLink = onError(({ graphQLErrors, networkError, response }) => {
+    if (graphQLErrors) {
+      // eslint-disable-next-line array-callback-return
+      graphQLErrors.map(({ message, locations, path }) => {
+        // eslint-disable-next-line no-console
+        console.error(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`);
+        setErrorMessage(message);
+      });
+    }
+
+    if (networkError) {
+      // eslint-disable-next-line no-console
+      console.error(`[Network error]: ${networkError}`);
+      setErrorMessage(networkError);
+    }
+    if (response) response.errors = null;
+  });
+
+  const httpLink = new HttpLink({
+    uri: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BASE_URL : 'http://localhost:3030/graphql',
+    cache,
+    headers: {
+      authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
+
+  const client = new ApolloClient({
+    cache,
+    link: errorLink.concat(httpLink),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Routes />
+      <ErrorSnackbar
+        open={showError}
+        onClose={() => { setShowError(false); setErrorMessage(''); }}
+        message={errorMessage}
+      />
+    </ApolloProvider>
+  );
+};
