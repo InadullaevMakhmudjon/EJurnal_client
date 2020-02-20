e5d80085758c57fdc3498277497e8465cb262c6d
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-02-21 02:24:01 +0500

    Author article title added

--- src/pages/author/Create.jsx
+++ src/pages/author/Create.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import style from 'assets/jss/pages/author';
+import style from 'assets/jss/pages/author/create';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import Stepper from 'components/author/Stepper';
+import Stepper from 'components/author/create/Stepper';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyle = makeStyles(style);
@@ -22,13 +22,13 @@ export default () => {
       <Typography variant="h4" className={classes.title}>Create new article</Typography>
       <Container className={classes.container}>
         <Grid container justify="center" direction="column" className={classes.wraper}>
-          <Typography variant="h4" align="center" className={classes.title}>Build Your Profile</Typography>
+          <Typography variant="h4" align="center" className={classes.title}>Build Your Journal</Typography>
           <Typography
             variant="body1"
             align="center"
             className={classes.title}
           >
-            This information will let us know more about you.
+            This information will let us know more about the journal.
           </Typography>
           <Stepper {...stepperProps} />
         </Grid>
