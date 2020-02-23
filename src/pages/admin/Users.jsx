cb272b34c3681f4799c271545bb5ce296567640f
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-02-23 10:49:59 +0500

    Users component modified with graphql queries

--- src/pages/admin/Users.jsx
+++ src/pages/admin/Users.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import {
+  Switch, Route, useRouteMatch,
+} from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
 import style from 'assets/jss/pages/admin/users';
 import Container from '@material-ui/core/Container';
 import Header from 'components/admin/users/Header';
 import Grid from '@material-ui/core/Grid';
 import Item from 'components/admin/users/Item';
 import Create from 'components/admin/users/Create';
+import Details from 'components/admin/users/Details';
+import { GET_USERS } from 'graphql/admin';
 
 const useStyle = makeStyles(style);
 
@@ -14,25 +19,27 @@ export default () => {
   // eslint-disable-next-line no-unused-vars
   const classes = useStyle();
   const match = useRouteMatch();
+  const { data, loading, refetch } = useQuery(GET_USERS, { variables: { roleId: '003' } });
 
-  const Users = (
+  const Users = loading ? <>Loading...</> : (
     <Grid container>
       {
-              [1, 2, 3, 5, 6].map((item) => (
-                <Grid key={item} item lg={3} md={4}>
-                  <Item />
-                </Grid>
-              ))
+        data ? data.users.map((user) => (
+          <Grid key={user.userName} item lg={3} md={4}>
+            <Item user={user} refetch={refetch} />
+          </Grid>
+        )) : <>HH</>
       }
     </Grid>
   );
 
   return (
     <Container maxWidth="xl">
-      <Header />
+      <Header showBack={!match.isExact} />
       <Switch>
-        <Route exact path={`${match.url}/`} render={() => Users} />
+        <Route exact path={`${match.url}/`} render={() => { refetch(); return Users; }} />
         <Route path={`${match.url}/create`} component={Create} />
+        <Route path={`${match.url}/:id`} component={Details} />
       </Switch>
 
     </Container>
